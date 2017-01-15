d22e8d39dc4ab68019ecb399de3734bfd6913371
kytenz <kai@ytterberg.com>
2017-01-15 14:52:14 +0100

    Changed comment

--- getCoop.sh
+++ getCoop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ITEMSEARCH=$(echo $1 | sed -e 's/ /\%20/g')
 echo "You searched for $ITEMSEARCH"
-# Only gets kyckling at the moment
+# Get the lowest price of the value (pricePerPc)
 GETPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep pricePerPc | sed -e 's/pricePerPc://g' | awk '{printf("%d\n",$1 + 0.5)}' | sort -nu | sed -n -e 's/^\([0-9]*\).*/\1/' -e '1p')
 echo "$1: $GETPRICE SEK"
 
