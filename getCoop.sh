511a9b84322116c46a0ed9fd7cae220e82d474fa
Kai Ytterberg <kaiytterberg@spotify.com>
2017-01-16 17:06:39 +0100

    Rewrite swedish letters to aao

--- getCoop.sh
+++ getCoop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ITEMSEARCH=$(echo $1 | sed -e 's/ /\%20/g')
+# Rewrite search to set %20 instead of space and swedish letters to aaoAAO
+ITEMSEARCH=$(echo $1 | sed -e 's/ /\%20/g' | tr '[åäöÅÄÖ]' '[aaoAAO]')
 echo "You searched for $ITEMSEARCH"
-# TODO: Rewrite ö => o, åä => a
 # Get the lowest price of the value (pricePerPc)
 GETPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep pricePerPc | sed -e 's/pricePerPc://g' | sort -nu | awk 'NR==1{print $1}')
 GETCOMPAREPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | grep "$GETPRICE")
