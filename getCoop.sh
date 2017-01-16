3b227f0d75db4f3537334c983e8dfef87bba2586
kytenz <kai@ytterberg.com>
2017-01-16 09:07:16 +0100

    Started work on getting other values from the chosen search item

--- getCoop.sh
+++ getCoop.sh
@@ -2,8 +2,13 @@
 ITEMSEARCH=$(echo $1 | sed -e 's/ /\%20/g')
 echo "You searched for $ITEMSEARCH"
 # Get the lowest price of the value (pricePerPc)
-GETPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep pricePerPc | sed -e 's/pricePerPc://g' | awk '{printf("%d\n",$1 + 0.5)}' | sort -nu | sed -n -e 's/^\([0-9]*\).*/\1/' -e '1p')
-echo "$1: $GETPRICE SEK"
+#GETPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep pricePerPc | sed -e 's/pricePerPc://g' | awk '{printf("%d\n",$1 + 0.5)}' | sort -nu | sed -n -e 's/^\([0-9]*\).*/\1/' -e '1p')
+GETPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep pricePerPc | sed -e 's/pricePerPc://g' | sort -nu | awk 'NR==1{print $1}')
+GETCOMPAREPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | grep "$GETPRICE")
+GETCOMPAREPRICE=$(echo $GETCOMPAREPRICE | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep comparativePrice | sed -e 's/comparativePrice://g')
+echo $GETCOMPAREPRICE
+echo "Compare price: $GETCOMPAREPRICE"
+echo "$1: $GETPRICE SEK" # (Jmfr. pris: $GETCOMPAREPRICE)"
 
 
 
