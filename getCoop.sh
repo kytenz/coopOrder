fd2c7a4d3ca4a770baa604ad53a195d881701867
Kai Ytterberg <kaiytterberg@spotify.com>
2017-01-16 16:55:38 +0100

    Added compare price of item

--- getCoop.sh
+++ getCoop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ITEMSEARCH=$(echo $1 | sed -e 's/ /\%20/g')
 echo "You searched for $ITEMSEARCH"
+# TODO: Rewrite ö => o, åä => a
 # Get the lowest price of the value (pricePerPc)
-#GETPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep pricePerPc | sed -e 's/pricePerPc://g' | awk '{printf("%d\n",$1 + 0.5)}' | sort -nu | sed -n -e 's/^\([0-9]*\).*/\1/' -e '1p')
 GETPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep pricePerPc | sed -e 's/pricePerPc://g' | sort -nu | awk 'NR==1{print $1}')
 GETCOMPAREPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$1" | sed -e 's/<[^>]*>//g' | grep "$GETPRICE")
-GETCOMPAREPRICE=$(echo $GETCOMPAREPRICE | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep comparativePrice | sed -e 's/comparativePrice://g')
-echo $GETCOMPAREPRICE
+GETCOMPAREPRICE=$(echo $GETCOMPAREPRICE | sed -e 's/[}"]*\(.\)[{"]*/\1/g;y/,/\n/' | grep comparativePrice | sed -e 's/comparativePrice://g' | sort -nu | awk 'NR==1{print $1}')
+#echo $GETCOMPAREPRICE
 echo "Compare price: $GETCOMPAREPRICE"
 echo "$1: $GETPRICE SEK" # (Jmfr. pris: $GETCOMPAREPRICE)"
 
