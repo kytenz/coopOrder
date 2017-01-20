9c4d972489893aa37ff3342d5f03d4f4c4f19616
kytenz <kai@ytterberg.com>
2017-01-20 23:05:24 +0100

    Slack integration are working as a proof of concept (WIP)

--- coopSlackIntegration.sh
+++ coopSlackIntegration.sh
@@ -3,26 +3,35 @@
 # Rewrite search to set %20 instead of space and swedish letters to aaoAAO
 ITEM=$1
 ITEMSEARCH=$(echo $ITEM | sed -e 's/ /\%20/g' | tr '[åäöÅÄÖ]' '[aaoAAO]' | sed -e 's/ar$//g' | sed -e 's/e$//g' | sed -e 's/na$//g')
-#echo "You searched for $ITEMSEARCH"
 
 # Get the lowest priced item (Used to then filter on the item for the lowest priced item) 
 # TODO: Maybe change to comparativePrice and not lowest price? Depends on size the user wants also
-GETLOWPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$ITEM" | sed -e 's/<[^>]*>//g' | sed 's/\ //g' | sed -n '/^{/p' | jq -r '.pricePerPc' | sort -nu | awk 'NR==1{print $1}')
-#echo "GET LOW PRICE: $GETLOWPRICE"
+GETLOWPRICE=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$ITEM" | sed -e 's/<[^>]*>//g' | sed 's/\ //g' | sed -n '/^{/p' | /usr/local/bin/jq -r '.pricePerPc' | sort -nu | awk 'NR==1{print $1}')
 
 # Get all information about the lowest priced item (json format)
 # TODO: Get more than just one hit, maybe top 3?
 GETITEM=$(curl -s -X GET -H "Content-type: application/json" -H "Accept: application/json"  https://www.coop.se/handla-online/sok/"$ITEMSEARCH" | grep -i "$ITEM" | sed -e 's/<[^>]*>//g' | grep -m 1 ":$GETLOWPRICE,")
 #echo $GETITEM
 DATE=$(date +%Y%m%d%H%M%S)
-GETCOMPAREPRICE=$(echo $GETITEM | jq -r '.comparativePrice')
-GETNAME=$(echo $GETITEM | jq -r '.priceUnit')
-GETNAME=$(echo $GETITEM | jq -r '.name')
-GETMANUFACTURER=$(echo $GETITEM | jq -r '.manufacturer')
+GETCOMPAREPRICE=$(echo $GETITEM | /usr/local/bin/jq -r '.comparativePrice')
+GETNAME=$(echo $GETITEM | /usr/local/bin/jq -r '.priceUnit')
+GETNAME=$(echo $GETITEM | /usr/local/bin/jq -r '.name')
+GETMANUFACTURER=$(echo $GETITEM | /usr/local/bin/jq -r '.manufacturer')
 #echo "$ITEM: $GETLOWPRICE SEK (Jmfr. pris: $GETCOMPAREPRICE/$GETUNIT)"
 #echo "$GETNAME ($GETMANUFACTURER): $GETLOWPRICE SEK (Jmfr. pris: $GETCOMPAREPRICE/$GETUNIT)"
 JSONFILE="test.json"
 > $JSONFILE
+echo "{"
+echo "    \"text\": \"$GETNAME ($GETMANUFACTURER): $GETLOWPRICE SEK (Jmfr. pris: $GETCOMPAREPRICE/$GETUNIT)\","
+echo "    \"attachments\": ["
+echo "        {"
+echo "            \"text\":\"$DATE\""
+echo "        }"
+echo "    ]"
+echo "}"
+
+
+
 echo "{" >> $JSONFILE
 echo "    \"text\": \"$GETNAME ($GETMANUFACTURER): $GETLOWPRICE SEK (Jmfr. pris: $GETCOMPAREPRICE/$GETUNIT)\"," >> $JSONFILE
 echo "    \"attachments\": [" >> $JSONFILE
@@ -32,6 +41,9 @@ echo "        }" >> $JSONFILE
 echo "    ]" >> $JSONFILE
 echo "}" >> $JSONFILE
 
+TEXTFILE="output.txt"
+> $TEXTFILE
+echo "$GETNAME ($GETMANUFACTURER): $GETLOWPRICE SEK (Jmfr. pris: $GETCOMPAREPRICE/$GETUNIT)" >> $TEXTFILE
 
 # Old code
 # Get lowest price from coop (2017-01-16)
--- runscript.php
+++ runscript.php
@@ -1,7 +1,12 @@
 <?php
-    /* echo 'Hello ' . htmlspecialchars($_GET["text"]) . '!';*/
+/* echo 'Hello ' . htmlspecialchars($_GET["text"]) . '!';*/
+/* TODO: The $text variable does not work when sending multiple words? */
     $text = $_GET["text"];
-    shell_exec("./coopSlackIntegration.sh $text");
-    $data = file_get_contents('test.json');
+    $text=escapeshellarg($text);
+    shell_exec('./coopSlackIntegration.sh '.$text.'');
+    
+    /* $data = file_get_contents('test.json'); */
+    $data = file_get_contents('output.txt');
+    /* json_string = json_encode($data, JSON_PRETTY_PRINT); */
     echo $data;
 ?>
