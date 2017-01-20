2356a2f426a5d1b51647e9998ffb936a531ab04e
Kai Ytterberg <kaiytterberg@spotify.com>
2017-01-20 11:56:20 +0100

    Small fix for php script

--- runscript.php
+++ runscript.php
@@ -2,6 +2,6 @@
     /* echo 'Hello ' . htmlspecialchars($_GET["text"]) . '!';*/
     $text = $_GET["text"];
     shell_exec("./coopSlackIntegration.sh $text");
-    $data = file_get_contents('./test.json');
+    $data = file_get_contents('test.json');
     echo $data;
 ?>
